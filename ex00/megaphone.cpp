4dac090c4d9abedc77fc072829d2cb7f2f26f29c
dayano74 <kiixxn00@gmail.com>
2025-08-14 20:10:41 +0900

    refactor: clean up Makefile and improve formatting in megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+#include <cctype>
 #include <cstring>
+#include <iostream>
 
-void megaphone(int cnt, char *argv[]){
-	if(cnt == 1){
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return;
-	}
-	for(int i = 1; i < cnt; i++){
-		for(size_t j = 0; j < strlen(argv[i]); j++){
-			std::cout << (char)toupper(argv[i][j]);
-		}
-	}
-	std::cout << std::endl;
+void megaphone(int cnt, char* argv[]) {
+  if (cnt == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return;
+  }
+  for (int i = 1; i < cnt; i++) {
+    for (size_t j = 0; j < strlen(argv[i]); j++) {
+      unsigned char ch = static_cast<unsigned char>(argv[i][j]);
+      std::cout << static_cast<char>(std::toupper(ch));
+    }
+  }
+  std::cout << std::endl;
 }
 
-int main(int argc, char* argv[]){
-	megaphone(argc, argv);
-	return 0;
+int main(int argc, char* argv[]) {
+  megaphone(argc, argv);
+  return 0;
 }
