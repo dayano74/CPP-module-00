42cda62eb2917500bc8ef8a399909485c39960e1
dayano74 <kiixxn00@gmail.com>
2025-08-23 17:14:46 +0900

    refactor: improve output formatting in megaphone function

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,12 +4,12 @@
 
 void megaphone(int cnt, char* argv[]) {
   if (cnt == 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" << std::endl;
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
     return;
   }
   for (int i = 1; i < cnt; i++) {
-	const char* s = argv[i];
-	const std::size_t len = std::strlen(s);
+    const char* s = argv[i];
+    const std::size_t len = std::strlen(s);
     for (size_t j = 0; j < len; j++) {
       unsigned char ch = static_cast<unsigned char>(s[j]);
       std::cout << static_cast<char>(std::toupper(ch));
